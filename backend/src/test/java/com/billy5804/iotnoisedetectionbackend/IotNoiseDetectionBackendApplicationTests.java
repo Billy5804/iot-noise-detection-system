7d59982d91f7b8bf899de53282eeabd34b1abfe7
Alex Royle <aj.royle@live.co.uk>
2022-03-20 13:38:30 +0000

    Added spring project with initial dep

---
+++ backend/src/test/java/com/billy5804/iotnoisedetectionbackend/IotNoiseDetectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billy5804.iotnoisedetectionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotNoiseDetectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
