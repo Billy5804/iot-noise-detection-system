ce90a64c6c10c315a6e0baa886a15976aa6fe12e
Alex Royle <aj.royle@live.co.uk>
2022-03-27 23:03:26 +0100

    Added methods to SiteUser Repo for getting SiteUsers using the exclude site projection along with excluding owners from results.

--- backend/src/main/java/com/billy5804/iotnoisedetectionbackend/repository/SiteUserRepository.java
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/repository/SiteUserRepository.java
@@ -1,11 +1,14 @@
 package com.billy5804.iotnoisedetectionbackend.repository;
 
 import java.util.Base64;
+import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.billy5804.iotnoisedetectionbackend.model.SiteUser;
 import com.billy5804.iotnoisedetectionbackend.model.SiteUserPK;
+import com.billy5804.iotnoisedetectionbackend.model.SiteUserRole;
+import com.billy5804.iotnoisedetectionbackend.projection.SiteUserExcludeSiteProjection;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called siteUserRepository
 // CRUD refers Create, Read, Update, Delete
@@ -15,5 +18,11 @@ public interface SiteUserRepository extends CrudRepository<SiteUser, SiteUserPK>
 	
 	default Iterable<SiteUser> getByUserId(String userId) {
 		return findBySiteUserPKUserId(Base64.getDecoder().decode(userId));
+	}
+	
+	Iterable<SiteUserExcludeSiteProjection> findBySiteUserPKSiteIdAndRoleNot(UUID siteId, SiteUserRole role);
+	
+	default Iterable<SiteUserExcludeSiteProjection> getNonOwnersBySiteId(UUID siteId) {
+		return findBySiteUserPKSiteIdAndRoleNot(siteId, SiteUserRole.OWNER);
 	}
 }
