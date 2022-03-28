715970988037093866bf15b896b29a97b6e728b4
Alex Royle <aj.royle@live.co.uk>
2022-03-28 16:47:44 +0100

    Updated SiteManagment Users modal body view: - Added loading view. - Added loadingError. - Added form with error view and submit button to remove unauthed site users. - Limited table size to 5 by default. - Added computed value used to decide wether to show remove unathed users form.

--- frontend/src/views/SiteManagement/SiteUsersView.vue
+++ frontend/src/views/SiteManagement/SiteUsersView.vue
@@ -1,11 +1,15 @@
 <script>
-import { ref, onBeforeMount } from "vue";
+import { ref, onBeforeMount, computed, shallowRef } from "vue";
 import { useUserStore } from "@/stores/UserStore";
 import axios from "axios";
-import BootstrapTable from "../../components/BootstrapTable.vue";
+import SiteRoles from "@/utilitys/SiteRoles";
+import BootstrapTable from "@/components/BootstrapTable.vue";
+import { MDBRow, MDBCol } from "mdb-vue-ui-kit";
+import AjaxButton from "@/components/AjaxButton.vue";
+import LoadingView from "../LoadingView.vue";
 
 export default {
-  components: { BootstrapTable },
+  components: { BootstrapTable, MDBRow, MDBCol, AjaxButton, LoadingView },
 
   props: {
     siteId: {
@@ -21,6 +25,10 @@ export default {
     const loadingError = ref(null);
     const siteUsersAPIPath = "https://noise.alexroyle.com/api/v1/site-users";
     const siteUsers = ref([]);
+
+    const siteHasUnauthorisedUsers = computed(() =>
+      siteUsers.value.some(({ role }) => role === SiteRoles.UNAUTHORISED)
+    );
 
     onBeforeMount(async () => {
       const siteUsersResponse = await axios
@@ -48,19 +56,19 @@ export default {
         visible: false,
       },
       {
-        field: "options",
+        field: "remove",
         align: "center",
         formatter: (index, { removing }) =>
           `<button type="button" id="table-remove-user" class="btn text-danger shadow-none p-0 m-0" 
           title="Remove users from site"${removing ? " disabled" : ""}>
-          ${
-            removing
-              ? `<div class="spinner-grow spinner-grow-sm" role="status">
-            <span class="visually-hidden">Removing...</span>
-          </div>`
-              : '<i class="fas fa-user-minus fa-xl"></i>'
-          }
-        </button>`,
+            ${
+              removing
+                ? `<div class="spinner-grow spinner-grow-sm" role="status">
+              <span class="visually-hidden">Removing...</span>
+            </div>`
+                : '<i class="fas fa-user-minus fa-xl"></i>'
+            }
+          </button>`,
         events: {
           "click #table-remove-user": (event, field, { id }) => removeUser(id),
         },
@@ -88,9 +96,8 @@ export default {
       paginationPagesBySide: 1,
       paginationParts: ["pageSize", "pageList"],
       pageList: [10, 25, 50, 100, 200, "All"],
+      pageSize: 5,
     };
-
-    function removeUnauthorisedUsers() {}
 
     async function removeUser(userId) {
       const userIndex = siteUsers.value.findIndex(({ id }) => id === userId);
@@ -109,21 +116,89 @@ export default {
         });
     }
 
+    const syncing = ref(false);
+
+    const removeError = shallowRef(null);
+
+    async function submitRemoveForm() {
+      removeError.value = null;
+      syncing.value = true;
+      axios
+        .delete(`${siteUsersAPIPath}?unauthorised`, {
+          timeout: 5000,
+          headers: { authorization: `Bearer ${await getIdToken()}` },
+          params: { siteId: props.siteId },
+        })
+        .then(({ data }) => {
+          const deletedUserIds = data.reduce((result, { userId }) => {
+            result.push(userId);
+            return result;
+          }, []);
+          siteUsers.value = siteUsers.value.filter(
+            ({ id }) => !deletedUserIds.includes(id)
+          );
+        })
+        .catch((error) => (removeError.value = error.message || error))
+        .finally(() => (syncing.value = false));
+    }
+
     return {
+      loading,
+      loadingError,
       tableColumns,
       tableOptions,
       siteUsers,
+      siteHasUnauthorisedUsers,
+      submitRemoveForm,
+      removeError,
+      syncing,
     };
   },
 };
 </script>
 
 <template>
-  <BootstrapTable
-    :columns="tableColumns"
-    :data="siteUsers"
-    :options="tableOptions"
-  />
+  <div>
+    <div v-if="loading" class="p-5">
+      <LoadingView />
+    </div>
+    <div
+      v-else-if="loadingError"
+      class="alert alert-danger text-center p-2 mb-0 w-100"
+      role="alert"
+    >
+      {{ loadingError }}
+    </div>
+    <BootstrapTable
+      v-else
+      :columns="tableColumns"
+      :data="siteUsers"
+      :options="tableOptions"
+    />
+    <MDBRow
+      v-if="siteHasUnauthorisedUsers"
+      tag="form"
+      class="g-3 mb-3"
+      novalidate
+      @submit.prevent="submitRemoveForm"
+    >
+      <MDBCol md="12" v-if="removeError">
+        <div class="alert alert-danger text-center p-2 mb-0" role="alert">
+          {{ removeError }}
+        </div>
+      </MDBCol>
+      <MDBCol md="12">
+        <AjaxButton
+          color="danger"
+          type="submit"
+          size="lg"
+          class="w-100"
+          :syncing="syncing"
+          >Remove All Unauthorised Users</AjaxButton
+        >
+      </MDBCol>
+    </MDBRow>
+  </div>
 </template>
 
 <style></style>
