e4d62e39f0d3abd6cbf324edc2de885f172b0453
Alex Royle <aj.royle@live.co.uk>
2022-03-28 16:37:47 +0100

    Created a method in the site user repo for deleting unauthed users and returns iterable SiteUserOnlyUserIdProjection of the deleted users.

--- backend/src/main/java/com/billy5804/iotnoisedetectionbackend/repository/SiteUserRepository.java
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/repository/SiteUserRepository.java
@@ -4,12 +4,14 @@ import java.util.Base64;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.billy5804.iotnoisedetectionbackend.model.SiteUser;
 import com.billy5804.iotnoisedetectionbackend.model.SiteUserPK;
 import com.billy5804.iotnoisedetectionbackend.model.SiteUserRole;
 import com.billy5804.iotnoisedetectionbackend.projection.SiteUserExcludeSiteProjection;
 import com.billy5804.iotnoisedetectionbackend.projection.SiteUserExcludeUserProjection;
+import com.billy5804.iotnoisedetectionbackend.projection.SiteUserOnlyUserIdProjection;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called siteUserRepository
 // CRUD refers Create, Read, Update, Delete
@@ -25,5 +27,15 @@ public interface SiteUserRepository extends CrudRepository<SiteUser, SiteUserPK>
 	
 	default Iterable<SiteUserExcludeSiteProjection> getNonOwnersBySiteId(UUID siteId) {
 		return findBySiteUserPKSiteIdAndRoleNot(siteId, SiteUserRole.OWNER);
+	}
+	
+	Iterable<SiteUserOnlyUserIdProjection> findBySiteUserPKSiteIdAndRole(UUID siteId, SiteUserRole role);
+	@Transactional
+	void deleteBySiteUserPKSiteIdAndRole(UUID siteId, SiteUserRole role);
+
+	default Iterable<SiteUserOnlyUserIdProjection> deleteUnauthorisedBySiteId(UUID siteId) {
+		final Iterable<SiteUserOnlyUserIdProjection> usersToBeDeletedIds = findBySiteUserPKSiteIdAndRole(siteId, SiteUserRole.UNAUTHORISED);
+		deleteBySiteUserPKSiteIdAndRole(siteId, SiteUserRole.UNAUTHORISED);
+		return usersToBeDeletedIds;
 	}
 }
