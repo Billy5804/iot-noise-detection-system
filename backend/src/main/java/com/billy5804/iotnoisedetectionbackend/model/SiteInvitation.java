a30bca41124072ea760c377d801cf0662735b33d
Alex Royle <aj.royle@live.co.uk>
2022-03-29 22:08:20 +0100

    Added model for the Site Invitation table.

---
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/model/SiteInvitation.java
@@ -0,0 +1,93 @@
+package com.billy5804.iotnoisedetectionbackend.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class SiteInvitation extends CommonAttributes implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2674357561866711276L;
+
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "uuid2")
+	@Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
+	private UUID id;
+
+	@ManyToOne
+	@JoinColumn(name = "site_id", columnDefinition = "BINARY(16)", referencedColumnName = "id", updatable = false, nullable = false)
+	private Site site;
+	
+	@Column(columnDefinition = "INT", updatable = true, nullable = true)
+	private int availableUses;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(updatable = true, nullable = false)
+	private Date expiresAt;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	@JsonIgnore
+	public UUID getSiteId() {
+		return site.getId();
+	}
+
+	public void setSiteId(UUID id) {
+		if (site == null) {
+			this.site = new Site();
+		}
+		this.site.setId(id);
+	}
+	
+	public Site getSite() {
+		return site;
+	}
+
+	public void setSite(Site site) {
+		this.site = site;
+	}
+
+	public int getAvailableUses() {
+		return availableUses;
+	}
+
+	public void setAvailableUses(int availableUses) {
+		this.availableUses = availableUses;
+	}
+
+	public Date getExpiresAt() {
+		return expiresAt;
+	}
+	
+	public void setExpiresAt(Date expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+}
