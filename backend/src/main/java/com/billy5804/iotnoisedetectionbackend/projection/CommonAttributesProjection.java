44519ce2ba7eb4120a8da023ab07fd924ffcc28e
Alex Royle <aj.royle@live.co.uk>
2022-03-29 22:09:27 +0100

    Added a projection for CommonAttributes and updated projections that had common attributes to extend it.

---
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/projection/CommonAttributesProjection.java
@@ -0,0 +1,9 @@
+package com.billy5804.iotnoisedetectionbackend.projection;
+
+import java.util.Date;
+
+public interface CommonAttributesProjection {
+	public Date getCreatedAt();
+
+	public Date getUpdatedAt();
+}
--- backend/src/main/java/com/billy5804/iotnoisedetectionbackend/projection/SiteUserExcludeUserProjection.java
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/projection/SiteUserExcludeUserProjection.java
@@ -1,18 +1,12 @@
 package com.billy5804.iotnoisedetectionbackend.projection;
 
-import java.util.Date;
-
 import com.billy5804.iotnoisedetectionbackend.model.Site;
 import com.billy5804.iotnoisedetectionbackend.model.SiteUserRole;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public interface SiteUserExcludeUserProjection {
+public interface SiteUserExcludeUserProjection extends CommonAttributesProjection {
 	@JsonProperty("site")
 	public Site getSiteUserPKSite();
 
 	public SiteUserRole getRole();
-
-	public Date getCreatedAt();
-
-	public Date getUpdatedAt();
 }
