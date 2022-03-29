30563c6eec1f8618f99ef76537670a2ecead6c89
Alex Royle <aj.royle@live.co.uk>
2022-03-29 02:25:07 +0100

    Formatted

--- backend/src/main/java/com/billy5804/iotnoisedetectionbackend/repository/SiteUserRepository.java
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/repository/SiteUserRepository.java
@@ -18,23 +18,25 @@ import com.billy5804.iotnoisedetectionbackend.projection.SiteUserOnlyUserIdProje
 
 public interface SiteUserRepository extends CrudRepository<SiteUser, SiteUserPK> {
 	Iterable<SiteUserExcludeUserProjection> findBySiteUserPKUserId(byte[] userId);
-	
+
 	default Iterable<SiteUserExcludeUserProjection> getByUserId(String userId) {
 		return findBySiteUserPKUserId(Base64.getDecoder().decode(userId));
 	}
-	
+
 	Iterable<SiteUserExcludeSiteProjection> findBySiteUserPKSiteIdAndRoleNot(UUID siteId, SiteUserRole role);
-	
+
 	default Iterable<SiteUserExcludeSiteProjection> getNonOwnersBySiteId(UUID siteId) {
 		return findBySiteUserPKSiteIdAndRoleNot(siteId, SiteUserRole.OWNER);
 	}
-	
+
 	Iterable<SiteUserOnlyUserIdProjection> findBySiteUserPKSiteIdAndRole(UUID siteId, SiteUserRole role);
+
 	@Transactional
 	void deleteBySiteUserPKSiteIdAndRole(UUID siteId, SiteUserRole role);
 
 	default Iterable<SiteUserOnlyUserIdProjection> deleteUnauthorisedBySiteId(UUID siteId) {
-		final Iterable<SiteUserOnlyUserIdProjection> usersToBeDeletedIds = findBySiteUserPKSiteIdAndRole(siteId, SiteUserRole.UNAUTHORISED);
+		final Iterable<SiteUserOnlyUserIdProjection> usersToBeDeletedIds = findBySiteUserPKSiteIdAndRole(siteId,
+				SiteUserRole.UNAUTHORISED);
 		deleteBySiteUserPKSiteIdAndRole(siteId, SiteUserRole.UNAUTHORISED);
 		return usersToBeDeletedIds;
 	}
