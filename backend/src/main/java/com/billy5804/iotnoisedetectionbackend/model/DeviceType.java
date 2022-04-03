bab5f7793617389c57dd3ba98b4e7d897c128ea9
Alex Royle <aj.royle@live.co.uk>
2022-04-03 21:39:45 +0100

    no message

---
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/model/DeviceType.java
@@ -0,0 +1,15 @@
+package com.billy5804.iotnoisedetectionbackend.model;
+
+public enum DeviceType {
+	NOISE("Noise level detector");
+	
+	private String label;
+	
+	DeviceType(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+}
