a805f2c9bd32333b7991977de629504cd7323b3b
Alex Royle <aj.royle@live.co.uk>
2022-04-23 01:29:26 +0100

    Disabled application destination /message prefix for WebSockets

--- backend/src/main/java/com/billy5804/iotnoisedetectionbackend/config/WebSocketConfig.java
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/config/WebSocketConfig.java
@@ -9,13 +9,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:5050").withSockJS();
-    }
- 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:5050").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
 //        registry.setApplicationDestinationPrefixes("/message");
-    }
+	}
 }
