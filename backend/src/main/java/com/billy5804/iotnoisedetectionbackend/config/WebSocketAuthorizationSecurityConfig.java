3cb3b45e7cd0244d3cdea1d0de42e4691c956aaa
Alex Royle <aj.royle@live.co.uk>
2022-04-23 01:44:58 +0100

    Added web socker authorization security config to make sure messages go through authentication.

---
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/config/WebSocketAuthorizationSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.billy5804.iotnoisedetectionbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+@Order(3)
+public class WebSocketAuthorizationSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+    @Override
+    protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
+        // You can customize your authorization mapping here.
+        messages.anyMessage().authenticated();
+    }
+    
+    // TODO: For testing purpose should re-enable this and provide a CRSF endpoint.
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
