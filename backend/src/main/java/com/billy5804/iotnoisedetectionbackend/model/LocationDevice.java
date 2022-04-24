f57f15cba86c02a530e7f72deecd81f817a1483e
Alex Royle <aj.royle@live.co.uk>
2022-04-24 14:31:47 +0100

    Added LocationDevice entity based off of the same table in the init sql file and using the LocationDevicePK as its id.

---
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/model/LocationDevice.java
@@ -0,0 +1,88 @@
+package com.billy5804.iotnoisedetectionbackend.model;
+
+import java.io.Serializable;
+import java.util.HexFormat;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+@Entity
+public class LocationDevice extends CommonAttributes implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6641260583715589068L;
+
+	@Id
+	private LocationDevicePK locationDevicePK = new LocationDevicePK();
+
+	@Column(columnDefinition = "int", updatable = true, nullable = true)
+	private Integer positionX;
+
+	@Column(columnDefinition = "int", updatable = true, nullable = true)
+	private Integer positionY;
+
+	@JsonIgnore
+	public LocationDevicePK getLocationDevicePK() {
+		return locationDevicePK;
+	}
+
+	public void setLocationDevicePK(LocationDevicePK locationDevicePK) {
+		this.locationDevicePK = locationDevicePK;
+	}
+
+	public UUID getLocationId() {
+		return locationDevicePK.getLocationId();
+	}
+
+	public void setLocationId(UUID locationId) {
+		locationDevicePK.setlocationIdd(locationId);
+	}
+
+	@JsonIgnore
+	public byte[] getDeviceId() {
+		return locationDevicePK.getDeviceId();
+	}
+
+	@JsonGetter(value = "deviceId")
+	public String getDeviceIdHex() {
+		return HexFormat.of().formatHex(getDeviceId());
+	}
+
+	public void setDeviceId(byte[] deviceId) {
+		locationDevicePK.setDeviceId(deviceId);
+	}
+
+	@JsonSetter
+	public void setDeviceId(String deviceId) {
+		setDeviceId(HexFormat.of().parseHex(deviceId));
+	}
+
+	public Integer getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(Integer positionX) {
+		this.positionX = positionX;
+	}
+
+	public Integer getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(Integer positionY) {
+		this.positionY = positionY;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(locationDevicePK);
+	}
+}
