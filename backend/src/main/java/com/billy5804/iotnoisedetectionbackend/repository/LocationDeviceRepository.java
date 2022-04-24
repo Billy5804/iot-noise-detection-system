568e38a090248d5a3633e2c4977653927bd8e4b4
Alex Royle <aj.royle@live.co.uk>
2022-04-24 14:33:46 +0100

    Added CRUD repos for both of the Location models

---
+++ backend/src/main/java/com/billy5804/iotnoisedetectionbackend/repository/LocationDeviceRepository.java
@@ -0,0 +1,13 @@
+package com.billy5804.iotnoisedetectionbackend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.billy5804.iotnoisedetectionbackend.model.LocationDevice;
+import com.billy5804.iotnoisedetectionbackend.model.LocationDevicePK;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called siteRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface LocationDeviceRepository extends CrudRepository<LocationDevice, LocationDevicePK> {
+
+}
