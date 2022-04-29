1d93d2e44a52a165feb98242c60b79093ec64dbe
Alex Royle <aj.royle@live.co.uk>
2022-04-29 21:58:36 +0100

    Updated dashboard device overview view to use the device card component.

--- frontend/src/views/Dashboard/Device/OverviewView.vue
+++ frontend/src/views/Dashboard/Device/OverviewView.vue
@@ -5,7 +5,6 @@ import {
   MDBCard,
   MDBCardHeader,
   MDBCardBody,
-  MDBCardText,
   MDBIcon,
   MDBBtn,
   MDBModal,
@@ -14,9 +13,10 @@ import {
   MDBModalBody,
   MDBModalFooter,
 } from "mdb-vue-ui-kit";
-import { ref, computed, onUnmounted } from "vue";
+import { computed } from "vue";
 import { RouterLink, RouterView, useRouter } from "vue-router";
 import DeviceOptionsView from "./OptionsView.vue";
+import DeviceCardView from "@/components/DeviceCardView.vue";
 import ForbiddenView from "../../ForbiddenView.vue";
 import SiteUserRoles from "@/utilitys/SiteUserRoles";
 
@@ -27,7 +27,6 @@ export default {
     MDBCard,
     MDBCardHeader,
     MDBCardBody,
-    MDBCardText,
     MDBIcon,
     MDBBtn,
     MDBModal,
@@ -39,6 +38,7 @@ export default {
     RouterView,
     DeviceOptionsView,
     ForbiddenView,
+    DeviceCardView,
   },
 
   props: {
@@ -81,72 +81,10 @@ export default {
       );
     });
 
-    const currentTime = ref(+new Date());
-
-    const currentTimeUpdate = setInterval(
-      () => (currentTime.value = +new Date()),
-      5000
-    );
-
-    onUnmounted(() => clearInterval(currentTimeUpdate));
-
-    function getSignalDetails(rssi, lastBeatTime) {
-      if (lastBeatTime + 900000 < currentTime.value) {
-        return {
-          text: "Offline",
-          icons: [
-            { color: "dark", icon: "signal" },
-            { color: "danger", icon: "slash" },
-          ],
-        };
-      }
-      if (rssi <= 0 && rssi >= -30) {
-        return {
-          text: "Adjacent to access point",
-          icons: [{ color: "success", icon: "signal" }],
-        };
-      }
-      if (rssi <= -31 && rssi >= -60) {
-        return {
-          text: "Excellent",
-          icons: [{ color: "success", icon: "signal" }],
-        };
-      }
-      if (rssi <= -61 && rssi >= -72) {
-        return { text: "OK", icons: [{ color: "warning", icon: "signal" }] };
-      }
-      if (rssi <= -73 && rssi >= -80) {
-        return { text: "Poor", icons: [{ color: "warning", icon: "signal" }] };
-      }
-      if (rssi <= -78 && rssi >= -94) {
-        return {
-          text: "Very poor",
-          icons: [{ color: "danger", icon: "signal" }],
-        };
-      }
-      if (rssi <= -95 && rssi >= -120) {
-        return {
-          text: "Unusable",
-          icons: [
-            { color: "danger", icon: "signal" },
-            { color: "dark", icon: "slash" },
-          ],
-        };
-      }
-      return {
-        text: "unknown",
-        icons: [
-          { color: "dark", icon: "signal" },
-          { color: "info", icon: "question" },
-        ],
-      };
-    }
-
     return {
       sortedSiteDevices,
       showModal,
       allowedModal,
-      getSignalDetails,
       SiteUserRoles,
     };
   },
@@ -184,47 +122,14 @@ export default {
         </RouterLink>
       </MDBCol>
       <MDBCol v-for="[deviceId, device] in sortedSiteDevices" :key="deviceId">
-        <MDBCard>
-          <MDBCardHeader class="d-flex">
-            <h5 class="text-truncate m-0" v-text="device.displayName" />
-            <span
-              class="position-relative ms-auto me-1"
-              :title="`Signal: ${
-                getSignalDetails(device.rssi, device.lastBeatTime).text
-              }\nRSSI: ${device.rssi}dBm`"
-            >
-              <MDBIcon
-                v-for="({ icon, color }, index) in getSignalDetails(
-                  device.rssi,
-                  device.lastBeatTime
-                ).icons"
-                :key="index"
-                :icon="icon"
-                :class="`${
-                  index > 0
-                    ? 'position-absolute top-50 start-50 translate-middle '
-                    : ''
-                }text-${color}`"
-              />
-            </span>
-            <DeviceOptionsView
-              :siteId="siteId"
-              :deviceId="deviceId"
-              :role="currentSiteRole"
-              class="d-flex"
-            />
-          </MDBCardHeader>
-          <MDBCardBody>
-            <MDBCardText
-              v-for="(sensor, sensorId) in device.sensors"
-              :key="sensorId"
-              class="text-end"
-            >
-              {{ sensor.latestValue + sensor.unit.getSymbol() }}
-              <MDBIcon :icon="sensor.unit.getSensorType().getIcon()" />
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
+        <DeviceCardView :device="device" :deviceId="deviceId">
+          <DeviceOptionsView
+            :siteId="siteId"
+            :deviceId="deviceId"
+            :role="currentSiteRole"
+            class="d-flex"
+          />
+        </DeviceCardView>
       </MDBCol>
       <RouterLink
         v-if="
