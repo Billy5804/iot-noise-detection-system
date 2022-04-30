1485b96c6d711f3195bf86d4058ec1787b6fbf25
Alex Royle <aj.royle@live.co.uk>
2022-04-30 02:58:03 +0100

    Updated instances of firebase storage getStorage to now use an exported version from the firebase database js that is init using the current firebase app instance.

--- frontend/src/utilitys/SVGOptimiseAndStore.js
+++ frontend/src/utilitys/SVGOptimiseAndStore.js
@@ -1,7 +1,6 @@
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { firebaseStorage } from "../firebase/database";
+import { ref, uploadBytes } from "firebase/storage";
 import { optimize as svgOptimise } from "svgo/dist/svgo.browser";
-
-const storage = getStorage();
 
 export async function svgOptimiseAndStore(file, filePath) {
   if (file.type !== "image/svg+xml") {
@@ -15,7 +14,7 @@ export async function svgOptimiseAndStore(file, filePath) {
     removeRasterImages: true,
   }));
 
-  const fileRef = ref(storage, filePath);
+  const fileRef = ref(firebaseStorage, filePath);
   const svgBlob = new Blob([optimisedSVGString], { type: "image/svg+xml" });
 
   await uploadBytes(fileRef, svgBlob);
