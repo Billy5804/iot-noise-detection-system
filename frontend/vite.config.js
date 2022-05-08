552e5353f9de6f4177e209bbbec8cc43b6ce7d50
Alex Royle <aj.royle@live.co.uk>
2022-05-08 16:03:24 +0100

    Updated vite config to include globals for the v1 api url and the ws url that change depending on production different for production and other deployment modes.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,21 @@
 import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-});
+  define: {
+    API_V1_URL: JSON.stringify(
+      mode === "production" ? "/api/v1/" : "http://localhost/api/v1/"
+    ),
+    WS_URL: JSON.stringify(
+      mode === "production" ? "/ws" : "http://localhost/ws"
+    ),
+  },
+}));
